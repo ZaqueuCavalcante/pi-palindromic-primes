a90191ede393d3be9ec8b01b678aa48de6da9db0
ZaqueuCavalcante <zaqueudovale@gmail.com>
2022-08-22 21:58:40 -0300

    Add get and find console apps

--- find_pal_primes/Program.cs
+++ find_pal_primes/Program.cs
@@ -1,1 +1,96 @@
-﻿
+﻿using System.Diagnostics;
+
+bool IsPrime(ulong number)
+{
+    if (number%2 == 0) return false;
+
+    var max = (ulong) Math.Sqrt(number);
+    for (ulong i = 3; i < max+1; i += 2)
+    {
+        if (number%i == 0) return false;
+    }
+    return true;
+}
+
+async Task<bool> GetFirstPalindromicPrime(int million, int digits)
+{
+    var myPi = "31415";
+    var billion = (int) (million / 1000);
+    var fileName = $"..\\pi_billion_{billion}_{billion+1}\\pi_million_{million}_{million+1}.txt";
+    myPi = await File.ReadAllTextAsync(fileName);
+
+    var endMax = myPi.Length - 1;
+    var stepToCenter = (int) (digits/2 - 1);
+
+    var start = 0;
+    var end = digits - 1;
+    var left = start + 1;
+    var right = end - 1;
+
+    var isPalindromic = false;
+
+    while (end < endMax)
+    {
+        if (myPi[end] is '1' or '3' or '7' or '9')
+        {
+            if (myPi[start] == myPi[end])
+            {
+                if (myPi[start+stepToCenter] == myPi[end-stepToCenter])
+                {
+                    isPalindromic = true;
+                    while (left != right)
+                    {
+                        if (myPi[left] == myPi[right])
+                        {
+                            left += 1;
+                            right -= 1;
+                        } else {
+                            isPalindromic = false;
+                            break;
+                        }   
+                    }
+                    if (isPalindromic)
+                    {
+                        var number = myPi.Substring(start, digits);
+                        Console.WriteLine($"palindromic={number} ---- in index={start} ---- of file=pi_million_{million}");
+                        if (IsPrime(ulong.Parse(number)))
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+
+        start += 1;
+        end = start + digits - 1;
+        left = start + 1;
+        right = end - 1;
+    }
+
+    return false;
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+
+var watch = Stopwatch.StartNew();
+Console.WriteLine("START");
+
+var million = 0;
+var digits = 19;
+
+var found = false;
+while (!found)
+{
+    found = await GetFirstPalindromicPrime(million, digits);
+    million ++;
+}
+
+watch.Stop();
+Console.WriteLine($"Duration = {watch.ElapsedMilliseconds/1000} seconds...");
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+
+
+
+
--- get_digits/Program.cs
+++ get_digits/Program.cs
@@ -1,1 +1,74 @@
-﻿
+﻿using System.Net;
+
+async Task GetOneMillionDigits(int start)
+{
+    using var client = new HttpClient();
+    var numberOfDigits = 1000;
+    var numberOfDigitsLong = (ulong) numberOfDigits;
+
+    var billion = (int) (start / 1000);
+    ulong startMillion = (ulong) start * 1_000_000;
+    var myPi = "";
+
+    HttpResponseMessage response;
+    string json;
+
+    for (int i = 0; i < 1000; i++)
+    {
+        try
+        {
+            do {
+                response = await client.GetAsync($"https://api.pi.delivery/v1/pi?start={startMillion}&numberOfDigits={numberOfDigits}&radix=10");
+            } while (response.StatusCode != HttpStatusCode.OK);
+        }
+        catch (System.Exception)
+        {
+            Console.WriteLine($"File with HTTP ERROR -> {billion}__{start}");
+            return;
+        }
+
+        json = await response.Content.ReadAsStringAsync();
+
+        myPi += json.Substring(12, numberOfDigits);
+
+        startMillion += numberOfDigitsLong;
+
+        if (i % 100 == 0)
+        {
+            Console.WriteLine($"File_{billion}__{start} -> {i/10}%");
+        }
+    }
+
+    Directory.CreateDirectory($"..\\pi_billion_{billion}_{billion+1}");
+    await File.WriteAllTextAsync($"..\\pi_billion_{billion}_{billion+1}\\pi_million_{start}_{start+1}.txt", myPi);
+
+    Console.WriteLine($"END -> {start}_{start+1}");
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+
+while (true)
+{
+    var tasks = new List<Task>();
+
+    var billion = 0;
+    var million = 33_000;
+    var filesToGet = 500;
+
+    for (int i = 0; i < filesToGet;)
+    {
+        billion = (int) (million / 1000);
+
+        var fileName = $"..\\pi_billion_{billion}_{billion+1}\\pi_million_{million}_{million+1}.txt";
+
+        if (!File.Exists(fileName)) {
+            tasks.Add(GetOneMillionDigits(million));
+            i++;
+        }
+
+        million ++;
+    }
+
+    Console.WriteLine($"Fetching {tasks.Count} files from PI API...");
+    await Task.WhenAll(tasks);
+}
